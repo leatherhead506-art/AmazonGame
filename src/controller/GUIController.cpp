b3aef3916fe6bcc7fc6fb27779102167cd42ec31
shellraiser <mehdi.belhachemi@etu.univ-amu.fr>
2026-06-25 15:10:32 +0200

    adding guiController

--- src/controller/GUIController.cpp
+++ src/controller/GUIController.cpp
@@ -1,2 +1,13 @@
 #include <SFML/Graphics.hpp>
+#include "GUIController.h"
 
+GuiController::GuiController(Player& player1,Player& player2, Board& board)
+{
+    board = Board(player1,player2);
+   
+}
+
+
+void GuiController::run(){
+
+}
---
+++ src/controller/GUIController.h
@@ -0,0 +1,27 @@
+#ifndef GUICONTROLLER_H
+#define GUICONTROLLER_H
+
+#include<vector>
+#include "../player/Player.h"
+#include "../model/board/Board.h"
+
+
+class GuiController{
+
+public:
+
+    GuiController(Player& player1, Player& player2, Board& board);
+
+
+
+    void run();
+
+
+
+
+private:
+
+
+};
+
+#endif
